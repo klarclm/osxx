cb7c756bd0107d19c25942d9f69ed7e3bc6b25f3
klarclm <klarclm@qq.com>
2014-08-13 22:18:17 +0800

    store服务中心增加dao层

---
+++ src/net/osxx/dao/StoreDao.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2005-2013 klarclm.com. All rights reserved.
+ * Support: http://www.klarclm.com
+ * License: http://www.klarclm.com/license
+ */
+package net.osxx.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import net.osxx.Filter;
+import net.osxx.Order;
+import net.osxx.Page;
+import net.osxx.Pageable;
+import net.osxx.entity.Article;
+import net.osxx.entity.ArticleCategory;
+import net.osxx.entity.Store;
+import net.osxx.entity.Tag;
+
+/**
+ * Dao - 文章
+ * 
+ * @author Operate System
+ * @version 3.0
+ */
+public interface StoreDao extends BaseDao<Store, Long> {
+
+	/**
+	 * 查找文章
+	 * 
+	 * @param articleCategory
+	 *            文章分类
+	 * @param tags
+	 *            标签
+	 * @param count
+	 *            数量
+	 * @param filters
+	 *            筛选
+	 * @param orders
+	 *            排序
+	 * @return 文章
+	 */
+	List<Article> findList(ArticleCategory articleCategory, List<Tag> tags, Integer count, List<Filter> filters, List<Order> orders);
+
+	/**
+	 * 查找文章
+	 * 
+	 * @param articleCategory
+	 *            文章分类
+	 * @param beginDate
+	 *            起始日期
+	 * @param endDate
+	 *            结束日期
+	 * @param first
+	 *            起始记录
+	 * @param count
+	 *            数量
+	 * @return 文章
+	 */
+	List<Article> findList(ArticleCategory articleCategory, Date beginDate, Date endDate, Integer first, Integer count);
+
+	/**
+	 * 查找文章分页
+	 * 
+	 * @param articleCategory
+	 *            文章分类
+	 * @param tags
+	 *            标签
+	 * @param pageable
+	 *            分页信息
+	 * @return 文章分页
+	 */
+	Page<Article> findPage(ArticleCategory articleCategory, List<Tag> tags, Pageable pageable);
+
+}
---
+++ src/net/osxx/dao/impl/StoreDaoImpl.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2005-2013 klarclm.com. All rights reserved.
+ * Support: http://www.klarclm.com
+ * License: http://www.klarclm.com/license
+ */
+package net.osxx.dao.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
+
+import net.osxx.Filter;
+import net.osxx.Order;
+import net.osxx.Page;
+import net.osxx.Pageable;
+import net.osxx.dao.ArticleDao;
+import net.osxx.dao.StoreDao;
+import net.osxx.entity.Article;
+import net.osxx.entity.ArticleCategory;
+import net.osxx.entity.Store;
+import net.osxx.entity.Tag;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Dao - 文章
+ * 
+ * @author Operate System
+ * @version 3.0
+ */
+@Repository("storeDaoImpl")
+public class StoreDaoImpl extends BaseDaoImpl<Store, Long> implements StoreDao {
+
+	public List<Article> findList(ArticleCategory articleCategory, List<Tag> tags, Integer count, List<Filter> filters, List<Order> orders) {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Article> criteriaQuery = criteriaBuilder.createQuery(Article.class);
+		Root<Article> root = criteriaQuery.from(Article.class);
+		criteriaQuery.select(root);
+		Predicate restrictions = criteriaBuilder.conjunction();
+		restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.equal(root.get("isPublication"), true));
+		if (articleCategory != null) {
+			restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.or(criteriaBuilder.equal(root.get("articleCategory"), articleCategory), criteriaBuilder.like(root.get("articleCategory").<String> get("treePath"), "%" + ArticleCategory.TREE_PATH_SEPARATOR + articleCategory.getId() + ArticleCategory.TREE_PATH_SEPARATOR + "%")));
+		}
+		if (tags != null && !tags.isEmpty()) {
+			Subquery<Article> subquery = criteriaQuery.subquery(Article.class);
+			Root<Article> subqueryRoot = subquery.from(Article.class);
+			subquery.select(subqueryRoot);
+			subquery.where(criteriaBuilder.equal(subqueryRoot, root), subqueryRoot.join("tags").in(tags));
+			restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.exists(subquery));
+		}
+		criteriaQuery.where(restrictions);
+		criteriaQuery.orderBy(criteriaBuilder.desc(root.get("isTop")));
+		return super.findList(criteriaQuery, null, count, filters, orders);
+	}
+
+	public List<Article> findList(ArticleCategory articleCategory, Date beginDate, Date endDate, Integer first, Integer count) {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Article> criteriaQuery = criteriaBuilder.createQuery(Article.class);
+		Root<Article> root = criteriaQuery.from(Article.class);
+		criteriaQuery.select(root);
+		Predicate restrictions = criteriaBuilder.conjunction();
+		restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.equal(root.get("isPublication"), true));
+		if (articleCategory != null) {
+			restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.or(criteriaBuilder.equal(root.get("articleCategory"), articleCategory), criteriaBuilder.like(root.get("articleCategory").<String> get("treePath"), "%" + ArticleCategory.TREE_PATH_SEPARATOR + articleCategory.getId() + ArticleCategory.TREE_PATH_SEPARATOR + "%")));
+		}
+		if (beginDate != null) {
+			restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.greaterThanOrEqualTo(root.<Date> get("createDate"), beginDate));
+		}
+		if (endDate != null) {
+			restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.lessThanOrEqualTo(root.<Date> get("createDate"), endDate));
+		}
+		criteriaQuery.where(restrictions);
+		criteriaQuery.orderBy(criteriaBuilder.desc(root.get("isTop")));
+		return super.findList(criteriaQuery, first, count, null, null);
+	}
+
+	public Page<Article> findPage(ArticleCategory articleCategory, List<Tag> tags, Pageable pageable) {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Article> criteriaQuery = criteriaBuilder.createQuery(Article.class);
+		Root<Article> root = criteriaQuery.from(Article.class);
+		criteriaQuery.select(root);
+		Predicate restrictions = criteriaBuilder.conjunction();
+		restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.equal(root.get("isPublication"), true));
+		if (articleCategory != null) {
+			restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.or(criteriaBuilder.equal(root.get("articleCategory"), articleCategory), criteriaBuilder.like(root.get("articleCategory").<String> get("treePath"), "%" + ArticleCategory.TREE_PATH_SEPARATOR + articleCategory.getId() + ArticleCategory.TREE_PATH_SEPARATOR + "%")));
+		}
+		if (tags != null && !tags.isEmpty()) {
+			Subquery<Article> subquery = criteriaQuery.subquery(Article.class);
+			Root<Article> subqueryRoot = subquery.from(Article.class);
+			subquery.select(subqueryRoot);
+			subquery.where(criteriaBuilder.equal(subqueryRoot, root), subqueryRoot.join("tags").in(tags));
+			restrictions = criteriaBuilder.and(restrictions, criteriaBuilder.exists(subquery));
+		}
+		criteriaQuery.where(restrictions);
+		criteriaQuery.orderBy(criteriaBuilder.desc(root.get("isTop")));
+		return super.findPage(criteriaQuery, pageable);
+	}
+
+}
